2c4ce65aceb2cb88a47ae3d0dc43bdc7185fe267
Anurag Bhat <anuragbhat@Anurags-MacBook-Air.local>
2025-06-01 10:52:09 -0400

    Final edits: config + package cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,3 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 export default {
-  output: 'export',
-  basePath: '/portfolio',
-  assetPrefix: isProd ? '/portfolio/' : '',
+  output: 'standalone',
 };
