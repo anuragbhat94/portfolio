852486248a9299f0dd767c5d758b555b4d16ed40
Anurag Bhat <anuragbhat@MacBookAir.lan>
2026-07-19 16:34:44 -0400

    Configure static export for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,13 @@
-export default {
-  output: 'standalone',
+const isProduction = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  output: "export",
+  basePath: isProduction ? "/portfolio" : "",
+  assetPrefix: isProduction ? "/portfolio/" : "",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
+
+export default nextConfig;
